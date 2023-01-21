f9c9c47495cb7354793e62cf0c0855e846abab26
Mark123M <mark123ymm@gmail.com>
2023-01-21 01:25:06 -0500

    first commit

--- main.cpp
+++ main.cpp
@@ -1,4 +1,10 @@
+#pragma comment (lib, "glew32s.lib")
+#define GLEW_STATIC
+#include <GL/glew.h>
 #include <glfw3.h>
+
+#include <iostream>
+
 
 int main(void)
 {
@@ -19,11 +25,31 @@ int main(void)
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
+    if (glewInit() != GLEW_OK) {
+        std::cout << "ERROR!" << std::endl;
+    }
+
+    std::cout << glGetString(GL_VERSION) << std::endl;
+
+    float positions[6] = {
+        -0.5f, -0.5,
+        0.0f, 0.5f,
+        0.5f, -0.5f
+    };
+
+    unsigned int buffer;
+    glGenBuffers(1, &buffer); //generate buffer and give back id
+    glBindBuffer(GL_ARRAY_BUFFER, buffer);
+    glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
+
+
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
+
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
